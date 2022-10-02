687803a123d6a1a8ceb9095546a3aca4e7c811d4
Anuda Weerasinghe <anuda@live.com>
2022-10-01 23:54:40 -0400

    search endpoint complete

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import politicianRouter from "./Politician";
 import billRouter from "./Bill";
 import stanceRouter from "./Stance";
+import searchRouter from "./Search";
 
 const router = express.Router();
 
 router.use("/politician", politicianRouter);
 router.use("/bill", billRouter);
 router.use("/stance", stanceRouter);
+router.use("/search", searchRouter);
 
 export default router;
