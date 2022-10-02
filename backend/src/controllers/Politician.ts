934a353cd8f74955e59bc1236c2d0411a7db85e6
Anuda Weerasinghe <anuda@live.com>
2022-10-01 21:45:36 -0400

    implements /politician

--- backend/src/controllers/Politician.ts
+++ backend/src/controllers/Politician.ts
@@ -1,11 +1,7 @@
 import { Request, Response } from "express";
-import { ObjectId } from "bson";
 import BillAuthorship from "models/BillAuthorship";
 import Politician from "models/Politician";
 import { bad, error } from "../util/error";
-import { AuthorType } from "_types/BillAuthorship"
-import { IPolitican } from "_types/Politician";
-import { IBill } from "_types/Bill";
 
 export const getPolitician = async (
   req: Request,
@@ -25,7 +21,10 @@ export const getPolitician = async (
         },
       });
 
-    res.
+    res.json({
+      politician: politician,
+      bills: bills,
+    });
   } catch (err) {
     if (err.name === "CastError" || err.name === "ValidationError") {
       return bad(res);
--- backend/src/routes/Politician.ts
+++ backend/src/routes/Politician.ts
@@ -1,5 +1,9 @@
+import { getPolitician } from "controllers/Politician";
 import express, { Router } from "express";
+import { asyncCatch } from "util/asyncCatch";
 
 const router: Router = express.Router();
+
+router.get("/:id", asyncCatch(getPolitician));
 
 export default router;
